d154ab6580de4d64a9d58f62e982a2071b80a647
Mohammad Nazzal <nazzalmohammad5@gmail.com>
2025-09-01 02:42:36 +0300

    working on navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,48 +3,45 @@ import { Link } from "react-router-dom";
 import "./header.css"
 
 function Header() {
-  const [navOpen, setNavOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  React.useEffect(() => {
-    if (navOpen) {
-      document.body.classList.add('menu-open');
-    } else {
-      document.body.classList.remove('menu-open');
-    }
-    return () => document.body.classList.remove('menu-open');
-  }, [navOpen]);
-
-  const handleHamburgerClick = () => {
-    setNavOpen((prev) => !prev);
-  };
-
-  const handleLinkClick = () => {
-    setNavOpen(false);
-  };
-
+  function toggleNav() {
+    setIsOpen(!isOpen);
+  }
+  
   return (
     <header className="header">
       <div className="logo-container">
-  <span className="logo"><img src="/coffee-lovers/photos/logo.png" alt="coffee logo" /></span>
+        <span className="logo"><img src="/coffee-lovers/photos/logo.png" alt="coffee logo" /></span>
         <span className="logo-name">Coffee Lovers</span>
       </div>
-      <div className="hamburger" onClick={handleHamburgerClick}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <div className={`navLinks${navOpen ? " active" : ""}`}>
-        <nav>
+      <div className="openList">
+          <button id="openListButton" onClick={toggleNav}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+      <div className="navLinks">
+        <nav className={`navLinks ${isOpen ? "active" : ""}`}>
           <ul className="nav-list">
-            <li><Link to="/" onClick={handleLinkClick}>Home</Link></li>
-            <li><Link to="/about" onClick={handleLinkClick}>About</Link></li>
-            <li><Link to="/services" onClick={handleLinkClick}>Services</Link></li>
-            <li><Link to="/contact" onClick={handleLinkClick}>Contact</Link></li>
+            <li>
+              <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+            </li>
+            <li>
+              <Link to="/about" onClick={() => setIsOpen(false)}>About</Link>
+            </li>
+            <li>
+              <Link to="/services" onClick={() => setIsOpen(false)}>Services</Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
+            </li>
           </ul>
         </nav>
       </div>
     </header>
   );
-}
+}1
 
 export default Header;
